e6cbfdc73d17320335b60b15dac99b9e49f8c9cc
Amar Myana <amar@securitybrigade.com>
2019-09-15 00:58:14 +0530

    Fixed till cloning the repositories locally

--- gitleaks.py
+++ gitleaks.py
@@ -1,194 +1,320 @@
 import os
 import json
 import urllib
-import hashlib
 import requests
 import traceback
-import subprocess
 
 
-from utils import default_output, run_command, make_folder, clone_repo, git_grep
-
-
-# Github api url from searching code
-API = "https://api.github.com/search/code?"
+from utils import run_command, mkdir
 
 
 class Gitleaks:
 
     def __init__(self, query, token):
-        self.query = query
+        self.query  = query
+        self.token  = token
+        
+        self.github = Github(self.token)
+    
+    def run(self):
+        print json.dumps(self._search(), indent=4)
+    
+    def _search(self):
+        
+        try:
+            repos = self._fetch_query_match_repositories()
+            
+            if not repos:
+                return None
+            
+            return self._clone_it(repos)
+            
+            # final_output = self.get_output(data, output)
+
+            # if not final_output:
+            #     return output
+            
+            # print final_output["data"]
+            # # Search in clone repo to get sensitive data
+            # matched = git_grep(final_output["data"])
+            # print matched
+            # if matched:
+            #     final_output["sensitive-info"] = matched
+            
+            # return final_output
+        
+        except Exception as e:
+            print str(e)
+            traceback.print_exc()
+    
+    # fetch query or keyword found repositories
+    def _fetch_query_match_repositories(self):
+        
+        response = self.github.search_in_code(self.query)
+        
+        if not response or "items" not in response:
+            return {}
+        
+        repos = []
+        items = response["items"]
+        
+        for item in items:
+            
+            if "repository" not in item:
+                continue
+            
+            repo = {}
+            
+            if "name" in item["repository"]:
+                repo["name"] = item["repository"]["name"]
+            
+            if "html_url" in item["repository"]:
+                repo["link"] = item["repository"]["html_url"]
+            
+            if "commits_url" in item["repository"]:
+                repo["last_commit"] = self.github.fetch_last_commit_hash(item["repository"]["commits_url"])
+            
+            print repo
+            
+            if repo not in repos:
+                repos.append(repo)
+            
+            break
+        
+        return repos
+    
+    def _clone_it(self, repositories):
+        
+        root = mkdir(self.query)
+        
+        if not root:
+            return None
+        
+        for repository in repositories:
+            path = Git().clone(repository, root)
+            repository['path'] = path
+        
+        return repositories
+    
+    # def get_output(self, data, output):
+    #
+    #     # Create a unique folder using sha1 hash of self.query
+    #     folder = make_folder(self.query)
+    #
+    #     if not folder:
+    #         return False
+    #
+    #     count, data = clone_repo(data, folder)
+    #
+    #     # No repositories is cloned
+    #     if count == 0:
+    #         return False
+    #
+    #     output["type"] = "success"
+    #     output["clone-path"] = folder
+    #
+    #     output["data"] = data
+    #     output["clone-stats"] = "%s of %s are cloned" % (count, len(data))
+    #
+    #     return output
+
+
+class Github:
+    
+    # Github API Endpoint
+    API = "https://api.github.com"
+    
+    def __init__(self, token):
         self.token = token
-
-    def send_request(self, url):
-
-        headers = {
-            "Authorization": "token %s" % (self.token)
-        }
-        response = requests.get(url, headers=headers, allow_redirects=False, timeout=20)
-
-        if response.status_code in range(200, 300) and 'json' in str(response.headers['content-type']):
+        
+        # authenticate takes up one request count
+        self._authenticate()
+    
+    def _authenticate(self):
+        
+        response = self._request_url(self.API)
+        
+        self._is_bad_token(response)\
+            ._is_rate_limit_exceeded(response)
+    
+    def _is_bad_token(self, response):
+        
+        if response.status_code == 401:
             content = json.loads(response.content)
-
-            # In case of pagination the header return in response consist of link key
-            # that usually have keys like last, previous and next which further have key named url
-            # {
-            #   'prev': {
-            #               'url': 'https://api.github.com/search/code?q=go&page=1',
-            #               'rel': 'prev'
-            #           },
-            #  'last': {
-            #               'url': 'https://api.github.com/search/code?q=go&page=34',
-            #               'rel': 'last'},
-            #  'first': {
-            #               'url': 'https://api.github.com/search/code?q=go&page=1',
-            #               'rel': 'first'
-            #           },
-            #   'next': {
-            #               'url': 'https://api.github.com/search/code?q=go&page=3',
-            #   'rel': 'next'
-            #           }
-            # }
-
-            if response.links and 'last' in response.links and 'url' in response.links['last']:
+        
+            if 'message' in content\
+                    and 'bad credentials' in content.message.lower():
+                raise BadTokenException("Invalid token: %s" % self.token, content)
+        
+        return self
+    
+    def _is_rate_limit_exceeded(self, response):
+        
+        if response.status_code == 403:
+            content = json.loads(response.content)
+            
+            if 'message' in content and content.message == 'api rate limit exceeded':
+                raise RateLimitExceededException("Rate Limit Exceeded for token: %s" % self.token, content)
+        
+        return self
+    
+    def search_in_code(self, query):
+        
+        url = self._prepare_search_in_code_url(query)
+        response = self._request_url(url)
+        
+        if response.status_code in range(200, 300):
+            
+            content = json.loads(response.content)
+            """
+            In case of pagination the header return in response consist of link key
+            that usually have keys like last, previous and next
+            which further have key named url
+            {
+                'prev': {
+                    'url': 'https://api.github.com/search/code?q=go&page=1',
+                    'rel': 'prev'
+                },
+                'last': {
+                    'url': 'https://api.github.com/search/code?q=go&page=34',
+                    'rel': 'last'},
+                'first': {
+                    'url': 'https://api.github.com/search/code?q=go&page=1',
+                    'rel': 'first'
+                },
+                'next': {
+                    'url': 'https://api.github.com/search/code?q=go&page=3',
+                    'rel': 'next'
+                }
+            }
+            """
+            
+            if response.links \
+                    and 'last' in response.links \
+                    and 'url' in response.links['last']:
+                
                 last_page = response.links['last']['url'].split('=')[-1]
+                
                 for page in range(2, int(last_page) + 1):
                     try:
-                        response = requests.get(url + "&page=%s" % (page), headers=headers, allow_redirects=False, timeout=20)
-                        if response.status_code in range(200, 300) and 'json' in str(response.headers['content-type']):
+                        response = self._request_url(url + "&page=%s" % page)
+                        
+                        if response.status_code in range(200, 300):
                             content.update(json.loads(response.content))
-                    except:
+                    except Exception as e:
+                        print str(e)
                         traceback.print_exc()
                         continue
+            
             return content
         
         return {}
-
-    def search_query(self):
-
-        query = urllib.urlencode({'q': '%s' % (self.query)})
-        url = API + query
-
-        return self.send_request(url)
-
-    def get_commit(self, commits_url):
-
-        commits_url = commits_url.replace("{/sha}", "/master")
-        contents    = self.send_request(commits_url)
-        
-        # Response of commits_url is a list of details of all commits
-        if not contents or "sha" not in contents:
-            return ""
-
-        return contents["sha"]
     
-    def get_contributors(self, contributors_url):
-        contents = self.send_request(contributors_url)
-        
-        # Response of contributors_url is a list of hashes of all contributors
-        if not contents:
-            return []
-        
-        contributors = []
-
-        for content in contents:
-            data = {}
-            if "login" in content:
-                data["name"] = content["login"]
-            if "html_url" in content:
-                data["link"] = content["html_url"]
-            if "contributions" in content:
-                data["contributions"] = content["contributions"]
-            
-            if "name" in data and "link" in data and "contributions" in data:
-                contributors.append(data)
-        
-        return contributors
-
-    def get_output(self, data, output):
-        
-        # Create a unique folder using sha1 hash of self.query
-        folder = make_folder(self.query)
-        
-        if not folder:
-            return False
-
-        count, data = clone_repo(data, folder)
-
-        # No repositories is cloned
-        if count == 0:
-            return False
-        
-        output["type"] = "success"
-        output["clone-path"] = folder
+    def _prepare_search_in_code_url(self, query):
+        return "%s/search/code?%s" % (self.API,
+                                      urllib.urlencode({'q': '%s' % query}))
     
-        output["data"] = data
-        output["clone-stats"] = "%s of %s are cloned" % (count, len(data))
-
-        return output
+    def _request_url(self, url):
+        
+        response = requests.get(url,
+                                headers=self._get_headers(),
+                                allow_redirects=False,
+                                timeout=30)
+        
+        return response
     
-    def get_repositories(self):
-
-        # Search query
-        content = self.search_query()
-
-        if not content or "items" not in content:
-            return {}
-
-        # Empty list to append the findings
-        data  = []
-        items = content["items"]
+    def _get_headers(self):
         
-        for item in items:
-            repo = {}
-            
-            if "repository" in item:
-                if "name" in item["repository"]:
-                    repo["name"] = item["repository"]["name"]
-                
-                if "html_url" in item["repository"]:
-                    repo["link"] = item["repository"]["html_url"]
-                
-                if "contributors_url" in item["repository"]:
-                    repo["contributors"] = self.get_contributors(item["repository"]["contributors_url"])
-                
-                if "commits_url" in item["repository"]:
-                    repo["commit"] = self.get_commit(item["repository"]["commits_url"])
-                
-                # Append only when all data required are found and if it is not in data
-                if sorted(["name", "link", "commit", "contributors"]) == sorted(list(repo)) and repo not in data:
-                    data.append(repo)
-            break
-        return data
-
-    def search(self):
+        return {
+            "Accept": "application / vnd.github.v3 + json",
+            "Authorization": "token %s" % self.token
+        }
+    
+    @staticmethod
+    def _is_ok_response(response):
         
-        output = json.loads(default_output())
-
-        try:
-            data = self.get_repositories()
-
-            if not data:
-                return output
-
-            final_output = self.get_output(data, output)
-
-            if not final_output:
-                return output
-            
-            print final_output["data"]
-            # Search in clone repo to get sensitive data
-            matched = git_grep(final_output["data"])
-            print matched
-            if matched:
-                final_output["sensitive-info"] = matched
-            
-            return final_output
-
-        except:
-            traceback.print_exc()
-            return output
-
-    def run(self):
+        return True if response.status_code in range(200, 300) else False
+    
+    def fetch_last_commit_hash(self, url):
         
-        return json.dumps((self.search()), indent=4)
+        commit_url  = url.replace("{/sha}", "")
+        response    = self._request_url(commit_url)
+        commit_sha  = ''
+        
+        if self._is_ok_response(response):
+            content     = json.loads(response.content)
+            last_commit = content[0]
+            
+            # Response of commits_url is a list of details of all commits
+            if "sha" in last_commit:
+                commit_sha = last_commit["sha"]
+        
+        return commit_sha
+
+
+class GithubException(Exception):
+    def __init__(self, message, response):
+        
+        super(GithubException, self).__init__(message)
+        self.response = response
+
+
+class BadTokenException(GithubException):
+    pass
+
+
+class RateLimitExceededException(GithubException):
+    pass
+
+
+class Git:
+    
+    # Git binary path
+    GIT_PATH = "git"
+    
+    def __init__(self):
+        pass
+    
+    def clone(self, repository, destination):
+        
+        repository_path  = os.path.join(destination, repository["name"])
+        
+        clone = run_command("%s clone %s %s" % (self.GIT_PATH,
+                                                "%s.git" % repository["link"],
+                                                repository_path))
+        
+        if not clone:
+            return FailedToCloneRepositoryException("Failed to clone the repository: %s" % repository['name'])
+        
+        last_commit = self.get_last_commit_hash(repository_path)
+        
+        if not last_commit or repository['last_commit'] != last_commit:
+            return FailedToCloneRepositoryException("Last commit doesn't match: Repository (%s) != Cloned (%s)"
+                                                    % (repository['last_commit'],
+                                                       last_commit))
+        
+        return repository_path
+    
+    def get_last_commit_hash(self, repository_path):
+        
+        if not os.path.exists(repository_path):
+            return None
+        
+        last_commit = run_command("%s -C %s rev-parse HEAD" % (self.GIT_PATH, repository_path))
+        
+        # run_command method returns bool type if failed
+        last_commit = last_commit.strip("\n") if type(last_commit) == str else ""
+        
+        # Hash of last commit and detected commit will be equate to avoid same commit
+        if not last_commit:
+            return None
+        
+        # HEAD means the repo is either not completely cloned or the git tree is not correct
+        if last_commit == "HEAD":
+            return None
+        
+        return last_commit
+
+
+class FailedToCloneRepositoryException(Exception):
+    pass
--- utils/__init__.py
+++ utils/__init__.py
@@ -1,156 +1,94 @@
-import re
 import os
 import json
 import subprocess
 import hashlib
 import traceback
 
-# Git binary path
-GIT_PATH = "/usr/bin/git"
-
 # Threads count required to clone repositories
 thread_count = 5
-
-
-def default_output():
-
-    return json.dumps({
-        "tool": "gitleaks",
-        "type": "failure",
-        "data": "",
-        "error": ""
-    })
 
 
 def run_command(command):
     
     try:
-        cmd    = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, executable='/bin/bash')
+        cmd    = subprocess.Popen(command,
+                                  shell=True,
+                                  stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                                  executable='C:\\Program Files\\Git\\bin\\bash')
         output = cmd.communicate()
         
         if cmd.returncode != 0 or output[1]:
             return False
         
-        return (output[0].strip("\n"))
-    except:
+        return output[0].strip("\n")
+    
+    except Exception as e:
+        print str(e)
         traceback.print_exc()
         return False
 
 
-def make_folder(file_name):
+def mkdir(file_name):
     
     path = os.path.join("/tmp", hashlib.sha1(file_name).hexdigest())
     
     if os.path.exists(path):
         return path
     
-    run_command("mkdir %s" % (path))
-
+    run_command("mkdir %s" % path)
+    
     if not os.path.exists(path):
         return False
     
     return path
 
 
-def clone_repo(repo_data, folder):
-        
-    # Count the clone success
-    count = 0
-    
-    for data in repo_data:
-        try:
-            link = data["link"] + ".git"
-            repo_name = link.split("/")[-1].replace(".git", "")
-            filename = os.path.join(folder, repo_name)
-            
-            # Check for last commit if repo already cloned already present using git rev-parse HEAD
-            # https://stackoverflow.com/questions/949314/how-to-retrieve-the-hash-for-the-current-commit-in-git
-            if os.path.exists(filename):
-                try:
-                    last_commit = run_command("%s -C %s rev-parse HEAD" % (GIT_PATH, filename))
-
-                    # run_command method returns bool type if failed
-                    last_commit = last_commit.strip("\n") if type(last_commit) == str else ""
-                    # Hash of last commit and detected commit will be equate to avoid same commit
-                    if last_commit and (data["commit"] == last_commit):
-                        print ("Not cloning %s " % (link))
-                        count += 1
-                        data["cloned"] = "True"
-                        data["cloned-path"] = filename
-                        continue
-
-                    # HEAD means the repo is either not completely cloned or the git tree is not correct
-                    if last_commit == "HEAD":
-                        run_command("rm -rf %s" % (filename))
-                    
-                    # Delete the file if exist but last commit is not equal to the 
-                    # cloned latest commit therefor reclone it.
-                    run_command("rm -rf %s" % (filename))
-
-                except:
-                    traceback.print_exc()
-                    continue
-                
-            clone = run_command("%s clone %s %s" % (GIT_PATH, link, filename))
-
-            if clone:
-                count += 1
-                data["cloned"] = "True"
-                data["cloned-path"] = filename
-
-        except:
-            traceback.print_exc()
-            continue
-
-    return count, repo_data
-
-
-def git_grep(repo_data):
-    
-    # Grep output to return
-    output = {}
-    # List of repo_path
-    repo_path = []
-
-    for data in repo_data:
-        if data.has_key("cloned-path"):
-            repo_path.append(data["cloned-path"])
-
-    # https://github.com/dxa4481/truffleHog/blob/dev/scripts/searchOrg.py#L10
-    search = {
-        "regex": {
-            "subdomain": r"/^[a-zA-Z0-9][a-zA-Z0-9.-]+[a-zA-Z0-9]$/",
-            "slack-token": r"(xox[p|b|o|a]-[0-9]{12}-[0-9]{12}-[0-9]{12}-[a-z0-9]{32})",
-            "aws-keys": r"AKIA[0-9A-Z]{16}",
-            "email-id": r"\S+@\S+"
-        },
-        "patterns": [
-            "password",
-            "credentials",
-            "username",
-            "*.api.*",
-            "*.key.*",
-            "token"
-        ]
-    }
-
-    for key, value in search["regex"].items():
-        match = []
-        for data in repo_data:
-            repo_path = data["cloned-path"]
-            result = run_command("%s -C %s grep -i %s" % (GIT_PATH, repo_path, value))
-            if result:
-                match.append(result)
-        if len(match) > 0:
-            output[key] = match
-
-    match = []
-    for value in search["patterns"]:
-        for data in repo_data:
-            repo_path = data["cloned-path"]
-            result = run_command("%s -C %s grep -i -e %s" % (GIT_PATH, repo_path, value))
-            if result and type(result) == dict:
-                match.append(result)
-    output["patterns-match"] = match
-
-    return output
+# def git_grep(repo_data):
+#
+#     # Grep output to return
+#     output = {}
+#     # List of repo_path
+#     repo_path = []
+#
+#     for data in repo_data:
+#         if data.has_key("cloned-path"):
+#             repo_path.append(data["cloned-path"])
+#
+#     # https://github.com/dxa4481/truffleHog/blob/dev/scripts/searchOrg.py#L10
+#     search = {
+#         "regex": {
+#             "subdomain": r"/^[a-zA-Z0-9][a-zA-Z0-9.-]+[a-zA-Z0-9]$/",
+#             "slack-token": r"(xox[p|b|o|a]-[0-9]{12}-[0-9]{12}-[0-9]{12}-[a-z0-9]{32})",
+#             "aws-keys": r"AKIA[0-9A-Z]{16}",
+#             "email-id": r"\S+@\S+"
+#         },
+#         "patterns": [
+#             "password",
+#             "credentials",
+#             "username",
+#             "*.api.*",
+#             "*.key.*",
+#             "token"
+#         ]
+#     }
+#
+#     for key, value in search["regex"].items():
+#         match = []
+#         for data in repo_data:
+#             repo_path = data["cloned-path"]
+#             result = run_command("%s -C %s grep -i %s" % (GIT_PATH, repo_path, value))
+#             if result:
+#                 match.append(result)
+#         if len(match) > 0:
+#             output[key] = match
+#
+#     match = []
+#     for value in search["patterns"]:
+#         for data in repo_data:
+#             repo_path = data["cloned-path"]
+#             result = run_command("%s -C %s grep -i -e %s" % (GIT_PATH, repo_path, value))
+#             if result and type(result) == dict:
+#                 match.append(result)
+#     output["patterns-match"] = match
+#
+#     return output
